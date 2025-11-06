949ecace9e82b4ad79afaa08035805119c6b490e
Simone Tiraboschi <stirabos@redhat.com>
2025-11-06 14:12:33 +0100

    Fix (again) icon automation
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- generate-fbc.sh
+++ generate-fbc.sh
@@ -128,7 +128,7 @@ case $cmd in
 	    yq "select( .package == \"$package_name\" or .name == \"$package_name\")" | \
       yq 'select(.schema != "olm.bundle" or .name == null or .name | capture("v4\.(?<minor>\d+)\.\d+") | .minor | to_number | . >= '${MIN_MINOR}')' | \
       yq 'select(.schema == "olm.bundle") = {"schema": .schema, "image": .image}' | \
-      yq 'select(.schema == "olm.package") = {"schema": .schema, "name": .name, "defaultChannel": .defaultChannel, "icon": strenv(ICON_BASE64), "description": strenv(DESCRIPTION)}' | \
+      yq 'select(.schema == "olm.package") = {"schema": .schema, "name": .name, "defaultChannel": .defaultChannel, "icon": {"base64data": strenv(ICON_BASE64), "mediatype": "image/svg+xml"}, "description": strenv(DESCRIPTION)}' | \
       yq 'select(.schema == "olm.channel") = {"entries": .entries | filter(.name | capture("v4\.(?<minor>\d+)\.\d+") | .minor | to_number | . >= '${MIN_MINOR}'), "name": .name, "package": .package, "schema": .schema}' | \
       yq '[.]' | \
       yq '{"schema": "olm.template.basic", "name": "kubevirt-hyperconverged", "entries":.}' | \
@@ -137,7 +137,7 @@ case $cmd in
       "jq")
 # shellcheck disable=SC2086,SC2046
         opm render $(opm_alpha_params "${frag}") "$from" | jq "select( .package == \"$package_name\" or .name == \"$package_name\")" | \
-            jq --arg icon "$ICON_BASE64" --arg description "$DESCRIPTION" 'if (.schema == "olm.bundle") then {schema: .schema, image: .image} else (if (.schema == "olm.package") then {schema: .schema, name: .name, defaultChannel: .defaultChannel, icon: $icon, description: $description} else . end) end' | \
+            jq --arg icon "$ICON_BASE64" --arg description "$DESCRIPTION" 'if (.schema == "olm.bundle") then {schema: .schema, image: .image} else (if (.schema == "olm.package") then {schema: .schema, name: .name, defaultChannel: .defaultChannel, icon: {base64data: $icon, mediatype: "image/svg+xml"}, description: $description} else . end) end' | \
             jq -s | \
             jq '{"schema": "olm.template.basic", "name": "kubevirt-hyperconverged", "entries": .}' > "${frag}"/graph.json
       ;;
